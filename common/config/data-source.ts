dbd531ab67e67b30147e75c371a565da99af636a
Ajinkya Devasthali <adevasthali@aaxis.io>
2025-10-24 14:58:10 +0530

    env and db config

---
+++ common/config/data-source.ts
@@ -0,0 +1,20 @@
+import { env } from './env';
+import { DataSourceOptions } from 'typeorm';
+import { DataSource } from 'typeorm/browser';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: env.pgdb.host,
+  port: env.pgdb.port,
+  username: env.pgdb.username,
+  password: env.pgdb.password,
+  database: env.pgdb.name,
+  entities: [],
+  migrations: [],
+  synchronize: false,
+  logging: false,
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
---
+++ common/config/env.ts
@@ -0,0 +1,12 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '../../.env' });
+
+export const env = {
+  pgdb: {
+    host: process.env.POSTGRES_HOST || 'localhost',
+    name: process.env.POSTGRES_DB_NAME || '',
+    username: process.env.POSTGRES_USERNAME || '',
+    password: process.env.POSTGRES_PASSWORD || '',
+    port: parseInt(process.env.POSTGRES_PORT || '5432'),
+  },
+};
