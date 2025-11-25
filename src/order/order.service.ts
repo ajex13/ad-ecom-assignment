bc723f7763d24f1294e15408d515bc22502235bd
Ajinkya Devasthali <adevasthali@aaxis.io>
2025-11-25 23:56:37 +0530

    priliminary crud for order entity

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,26 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { Repository } from 'typeorm';
+import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class OrderService {
+  constructor(
+    @InjectRepository(Order)
+    private orderRepository: Repository<Order>,
+  ) {}
   create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+    const order = this.orderRepository.create(createOrderDto);
+    return this.orderRepository.save(order);
   }
 
-  findAll() {
-    return `This action returns all order`;
+  async findAll() {
+    const orders = await this.orderRepository.find();
+
+    return plainToInstance(Order, orders, {
+      excludeExtraneousValues: true,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  async findOne(id: string) {
+    const order = await this.orderRepository.findOne({ where: { id } });
+
+    if (!order) {
+      throw new NotFoundException('Order not found');
+    }
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
+  async update(id: string, updateOrderDto: UpdateOrderDto) {
+    const order = await this.orderRepository.preload({
+      id: id,
+      ...updateOrderDto,
+    });
+
+    if (!order) {
+      throw new NotFoundException('Order not found');
+    }
+
+    const savedOrder = await this.orderRepository.save(order);
+
+    return plainToInstance(Order, savedOrder, {
+      excludeExtraneousValues: true,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+  remove(id: string) {
+    return this.orderRepository.delete(id);
   }
 }
